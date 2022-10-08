b32796b6f3a18e82c52f7a2f20d6c673c7c2acd9
CarluPtr <pietricarlu@hotmail.fr>
2022-10-08 12:16:35 +0200

    Admin pannel
    - Delete button
    - Certified Figure button
    
    Admin granted verification

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -29,4 +29,63 @@ class AdminController extends AbstractController
             'comments' => $commentRepository->findBy(array(), array('dateCreation' => 'DESC'))
         ]);
     }
+
+    /**
+     * @Route("/delete/figure/{id}", name="delete_figure")
+     */
+    public function deleteFigure(EntityManagerInterface $entityManager, $id): Response
+    {
+        // Verify if user is an admin and throw AccesDeniedException if he's not
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $repository = $entityManager->getRepository(Figure::class);
+        $figure = $repository->findOneBy(array('id' => $id));
+
+        $entityManager->remove($figure);
+        $entityManager->flush();
+
+        return $this->redirectToRoute("app_admin");
+    }
+
+    /**
+     * @Route("/delete/comment/{id}", name="delete_comment")
+     */
+    public function deleteComment(EntityManagerInterface $entityManager, $id): Response
+    {
+        // Verify if user is an admin and throw AccesDeniedException if he's not
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $repository = $entityManager->getRepository(Comment::class);
+        $comment = $repository->findOneBy(array('id' => $id));
+
+        $entityManager->remove($comment);
+        $entityManager->flush();
+
+        return $this->redirectToRoute("app_admin");
+    }
+
+    /**
+     * @Route("/certif/figure/{id}", name="certif_figure")
+     */
+    public function certifFigure(EntityManagerInterface $entityManager, $id): Response
+    {
+        // Verify if user is an admin and throw AccesDeniedException if he's not
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $repository = $entityManager->getRepository(Figure::class);
+        $figure = $repository->findOneBy(array('id' => $id));
+
+        if($figure->isCertified())
+        {
+            $figure->setCertified(0);
+        }
+        else
+        {
+            $figure->setCertified(1);
+        }
+        $entityManager->persist($figure);
+        $entityManager->flush();
+
+        return $this->redirectToRoute("app_admin");
+    }
 }
